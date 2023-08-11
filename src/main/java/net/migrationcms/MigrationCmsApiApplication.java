1b38d53993e991da210b33e0f4966a14a19df2af
Patrice-H <webmestre64@gmail.com>
2023-08-11 12:14:47 +0200

    Minimum configuration

---
+++ src/main/java/net/migrationcms/MigrationCmsApiApplication.java
@@ -0,0 +1,13 @@
+package net.migrationcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationCmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationCmsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/migrationcms/MigrationCmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.migrationcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationCmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
